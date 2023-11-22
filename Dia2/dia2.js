95c9df412e7c9ae93a774df6912a41393871c0ad
Guilherme Mainchein <guilhermesmotoc@gmail.com>
2023-11-21 21:52:31 -0300

    add um codigo no index dia 2 e testando ifs e while

---
+++ Dia2/dia2.js
@@ -0,0 +1,19 @@
+console.log("Olá mundo mto massa")
+var videogame = prompt("QUAL SEU VIDEOGAME FAVORITO? [PS / XBOX]")
+if (videogame == "PS")
+ console.log("UAU VOCE PREFERE OS EXCLUSIVOS AO INVES DE HARDWARE")
+if else (videogame == "XBOX")
+ console.log("AH ENTAO VOCE PREFERE HARDWERE E BONS SERVICOS AO INVES DE EXCLUSIVOS DE PESO")
+else 
+var rep = true
+console.log("Infelizmente a opcao escolhida nao esta sendo levada em conta tente responder novamente")
+
+while(rep == true){
+var videogame = prompt("QUAL SEU VIDEOGAME FAVORITO? [PS / XBOX]")
+if (videogame == "PS")
+ console.log("UAU VOCE PREFERE OS EXCLUSIVOS AO INVES DE HARDWARE")
+if else (videogame == "XBOX")
+ console.log("AH ENTAO VOCE PREFERE HARDWERE E BONS SERVICOS AO INVES DE EXCLUSIVOS DE PESO") 
+
+}
+
