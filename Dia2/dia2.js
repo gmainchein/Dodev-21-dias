fb09ec3a29413a5616abeed5defcd04f1c605d41
Guilherme Mainchein <guilhermesmotoc@gmail.com>
2023-11-21 22:06:32 -0300

    Agora o codigo usando while e if foi aplicado com sucesso

--- Dia2/dia2.js
+++ Dia2/dia2.js
@@ -1,19 +1,29 @@
+
+var rep = true;
 console.log("Olá mundo mto massa")
-var videogame = prompt("QUAL SEU VIDEOGAME FAVORITO? [PS / XBOX]")
-if (videogame == "PS")
- console.log("UAU VOCE PREFERE OS EXCLUSIVOS AO INVES DE HARDWARE")
-if else (videogame == "XBOX")
- console.log("AH ENTAO VOCE PREFERE HARDWERE E BONS SERVICOS AO INVES DE EXCLUSIVOS DE PESO")
-else 
-var rep = true
-console.log("Infelizmente a opcao escolhida nao esta sendo levada em conta tente responder novamente")
+var rep = true;
 
 while(rep == true){
-var videogame = prompt("QUAL SEU VIDEOGAME FAVORITO? [PS / XBOX]")
-if (videogame == "PS")
- console.log("UAU VOCE PREFERE OS EXCLUSIVOS AO INVES DE HARDWARE")
-if else (videogame == "XBOX")
- console.log("AH ENTAO VOCE PREFERE HARDWERE E BONS SERVICOS AO INVES DE EXCLUSIVOS DE PESO") 
 
+    var videogame = prompt("QUAL SEU VIDEOGAME FAVORITO? [PS / XBOX]").toUpperCase();
+
+    if (videogame == "PS") {
+
+        console.log("UAU VOCE PREFERE OS EXCLUSIVOS AO INVES DE HARDWARE");
+        rep = false;
+
+    }
+
+    else if (videogame == "XBOX") {
+
+        console.log("AH ENTAO VOCE PREFERE HARDWERE E BONS SERVICOS AO INVES DE EXCLUSIVOS DE PESO");
+        rep = false;
+
+    }
+
+    else {
+
+        console.log("Infelizmente a opcao escolhida nao esta sendo levada em conta tente responder novamente")
+
+    }
 }
-
