7984504e5c94d2e660fdf73f3c91592b2d3190b2
Guilherme Mainchein <guilhermesmotoc@gmail.com>
2023-11-23 17:10:32 -0300

    commit da aula 3 exercicios e aprendizados

--- dia3teste/exerciciodia3.js
+++ dia3teste/exerciciodia3.js
@@ -1,3 +1,22 @@
+console.log("============ CONST =========")
+console.log( a)
+const b = " const b = A segunda letra do alfabeto"
+console.log( b ) 
+
+console.log("========== LET =========")
+
+let meuSobrenome = "Mainchein"
+console.log( "nomeando a variavel let obtemos: " + meuSobrenome);
+let meuSobrenome2 = "Silva"
+console.log( "dando outro valor a váriavel let obtemos: " + meuSobrenome2);
+
+console.log("========== PROMPT ======")
+let fprompt = parseInt(prompt("INSIRA UM NUMERO DECIMAL"))
+console.log("usando o parseInt temos: " +fprompt);
+let sprompt = Number(prompt("INSIRA O MESMO NUMERO DECIMAL"))
+console.log( "usando a expressao 'number' temos : "+ sprompt);
+
+
 console.log("========== EXERCICIO DIA 3 ========")
 let peso = Number(prompt("INSIRA SEU PESO: (KG) "))
 let idade = Number(prompt("INSIRA SUA IDADE: "))
