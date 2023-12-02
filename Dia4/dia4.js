16209c1d14555b0ac6b0ee581796106a174f9006
Guilherme Mainchein <guilhermesmotoc@gmail.com>
2023-12-02 14:38:56 -0300

    commit do 2 exercicio e\ou

--- Dia4/dia4.js
+++ Dia4/dia4.js
@@ -13,3 +13,20 @@ if (fome === "nao" || dinheiro === "nao"){
     if( fome === "sim" && dinheiro === "sim" && restaurante === "aberto"){
         console.log("Hoje o jantar será no seu restaurante favorito! ")
     }
+
+
+    console.log(" ---------------- EXERCICIO 2 DIA 4 --------------------------")
+    let nome =     prompt(" Qual seu nome? ")
+    let idade =    prompt(" Qual sua idade? ")
+    let carteira = prompt(" Voce possui habilitacao? ")
+     let carro =   prompt(" Voce tem um carro? ")
+
+     if( idade < 18 || carteira === "nao"){
+          console.log(  nome ," ,voce é menor de idade e nao pode dirigir!")
+     }
+     if ( idade >= 18 && carteira === "sim" && carro === "nao"){
+          console.log(nome , " ,voce é maior de idade, pode dirigir porem nao tem um carro :( ")
+     }
+     if (idade >= 18 && carteira === "sim" && carro === "sim"){
+        console.log(nome , " voce sera o motorista!")
+     }
