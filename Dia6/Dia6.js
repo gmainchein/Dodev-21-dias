bd5aa9159c3131b3242a56ffafc29322b3d5c0fe
Guilherme Mainchein <guilhermesmotoc@gmail.com>
2023-12-06 21:07:38 -0300

    dia6 FOR

---
+++ Dia6/Dia6.js
@@ -0,0 +1,21 @@
+console.log("-----------------EXERCICIO DIA 6-------------------------")
+
+
+let contarate = prompt(" VOCE DESEJA CONTAR ATE QUAL NUMERO? ")
+for (contador = 0; contador <= contarate; contador++ ) {
+ console.log( contador)
+}
+
+
+console.log("------------QUESTAO 2--------------------------")
+
+for(contador2= 0; contador2 <= 50 ; contador2 = contador2 + 5 ){
+ console.log( contador2)
+}
+
+
+console.log("---------------QUESTAO 3 -----------------------")
+
+for(cont = 50; cont > 0 ; cont-= 5 ){
+    console.log(cont + "...")
+}
