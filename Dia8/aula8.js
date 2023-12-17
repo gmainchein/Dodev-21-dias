b4ee380c534c4b5daef7c3dfd9ffbe3070b7e3eb
Guilherme Mainchein <guilhermesmotoc@gmail.com>
2023-12-17 20:47:29 -0300

    dia 8 e calculadora dodev

---
+++ Dia8/aula8.js
@@ -0,0 +1,12 @@
+function soma(){
+    let num1 = Number(prompt("insira um número"))
+    let num2 = Number(prompt("insira outro numero"))
+    console.log( "O resultado da soma é : " ,  num1 + num2)
+}
+
+ function multiplicacao(){
+    let multi1 = Number(prompt("Insira um numero "))
+    let multi2 = Number(prompt("Insira um numero "))
+    console.log("o produto dos numeros foram de: ", multi1 * multi2 )
+ }
+ multiplicacao()
