5779b01b5166d5384391a62cc52dd8b433d2b14d
Guilherme Mainchein <guilhermesmotoc@gmail.com>
2023-12-28 15:33:25 -0300

    Array e Exercicios

--- Dia10/exerc4aula10.js
+++ Dia10/exerc4aula10.js
@@ -21,3 +21,16 @@ while (IndiceArrayInverso < ContarAte) {
 // LOOPING = ARRAYINVERSA [ indice 0] = ARRAYOG = [indice2] =  [ 3 - 1 - indice0 ] 
 // [ContarAte - 1 - IndiceArrayInverso] = [ 3 - 1 - 0 ]= 2  entao o INDICE2 da array original seria igual o Indice0 da Array inversa
 console.log("Array inversa: ", ArrayInversa);
+
+
+// Exercici0 5 DESAFIO
+
+let prompt_numero2 = Number(prompt("Insira um numero inteiro positivo"));
+let ArrayFibonacci = [];
+ArrayFibonacci[0] = prompt_numero2 - 1 ;
+ArrayFibonacci[1] = prompt_numero2 ;
+
+for(i = 2; i < 10; i++){
+ArrayFibonacci[i] = ArrayFibonacci[i - 1] + ArrayFibonacci[ i - 2];
+}
+console.log(ArrayFibonacci)
